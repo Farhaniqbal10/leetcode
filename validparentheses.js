8637223e79150b62ff4716007a4e79cb391b6a85
Farhan Iqbal <139185311+Farhaniqbal10@users.noreply.github.com>
2025-01-26 11:48:36 +0700

    Create validparentheses.js

---
+++ validparentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const kurungan = {
+        ')' : '(',
+        '}' : '{',
+        ']' : '['
+    }
+
+    const tumpuk = []
+
+    for(const char of s) {
+        if(char in kurungan){
+            const topElement = tumpuk.pop() || '#';
+            if(kurungan[char] !== topElement){
+                return false;
+            }
+        }else {
+            tumpuk.push(char);
+        }
+    }
+    return tumpuk.length === 0;
+};
